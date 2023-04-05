df4789ae15ec6b61801a2c7d326e1735e6f6c1ab
shikurassefa <jabirshikur@gmail.com>
2023-04-05 06:59:52 -0700

    changing an id value into number

--- tour-product.js
+++ tour-product.js
@@ -1,6 +1,6 @@
 const product = [
   {
-    id: "rec6d6T3q5EBIdCfD",
+    id: 0,
     name: "Aksum",
     info: "Aksum is a city in northern Ethiopia. It's known for its tall, carved obelisks, relics of the ancient Kingdom of Aksum. Most are in the northern Stelae Park, including a huge fallen pillar, now in pieces. Centuries-old St. Mary of Zion is a Christian church and pilgrimage site believed to have housed the biblical Ark of the Covenant. ",
     image:
@@ -8,7 +8,7 @@ const product = [
     price: "1,995",
   },
   {
-    id: "recIwxrvU9HfJR3B4",
+    id: 1,
     name: "Lalibela",
     info: "Lalibela is a town in the Amhara region of northern Ethiopia. It's known for its distinctive rock-cut churches dating from the 12th and 13th centuries, which are pilgrimage sites for Coptic Christians. Carved out of rock, the subterranean monoliths include huge Bete Medhane Alem, and cross-shaped Bete Giyorgis. ",
     image:
@@ -16,7 +16,7 @@ const product = [
     price: "3,895",
   },
   {
-    id: "recJLWcHScdUtI3ny",
+    id: 2,
     name: "Harar Jugol",
     info: "The fortified historic town of Harar is located in the eastern part of the country on a plateau with deep gorges surrounded by deserts and savannah. The walls surrounding this sacred Muslim city were built between the 13th and 16th centuries. Harar Jugol, said to be the fourth holiest city of Islam, numbers 82 mosques, three of which date from the 10th century, and 102 shrines, but the townhouses with their exceptional interior design constitute the most spectacular part of Harar's cultural heritage. The impact of African and Islamic traditions on the development of the town's building types and urban layout make for its particular character and uniqueness.",
     image:
@@ -24,7 +24,7 @@ const product = [
     price: "2,695",
   },
   {
-    id: "recK2AOoVhIHPLUwn",
+    id: 3,
     name: "Al Nejashi Mosque",
     info: "An-Najāshī), was the ruler of the Kingdom of Aksum who reigned from 614–631 C.E. He is primarily known through the coins that were minted during his reign.[Al Nejashi Mosque is a mosque in Negash, in the Tigray Region of Ethiopia. Named after Najashi, it is one of the oldest mosques in Africa.",
     image:
@@ -32,7 +32,7 @@ const product = [
     price: "2,095",
   },
   {
-    id: "receAEzz86KzW2gvH",
+    id: 4,
     name: "Ras Dashen",
     info: "Ras Dashen, also known as Ras Dejen, is the highest mountain in Ethiopia and fourteenth highest peak in Africa. Located in the Simien Mountains National Park in the North Gondar Zone of the Amhara Region, it reaches an elevation of 4,550 metres.",
     image: "https://cdn.tourradar.com/s3/tour/1500x800/155452_8181b581.jpg",
